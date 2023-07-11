fd70fccce18a4ff25f44a5040720cafc16a20d96
ZylDa <a3842772@gmail.com>
2023-07-11 22:51:18 +0800

    7/11

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import './pages/auth_page.dart';
-import 'package:swap/routes.dart';
 
 import 'screens/splash/splash_screen.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,12 +18,37 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      //debugShowCheckedModeBanner: false,
-      title: 'Swap Demo',
-      home: AuthPage(),
-      //initialRoute: SplashScreen.routeName,
-      //routes: routes,
-    );
+    return FutureBuilder<bool>(
+        future: checkFirstTimeUser(),
+        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            // 此时可以显示一个加载指示器或启动屏幕
+            return const CircularProgressIndicator();
+          } else {
+            if (snapshot.hasError) {
+              // 处理错误情况
+              return Text('Error: ${snapshot.error}');
+            } else {
+              bool isFirstTimeUser = snapshot.data ?? true;
+              return MaterialApp(
+                //debugShowCheckedModeBanner: false,
+                title: 'Swap Demo',
+                home: isFirstTimeUser ? SplashScreen() : const AuthPage(),
+              );
+            }
+          }
+        });
+  }
+
+  Future<bool> checkFirstTimeUser() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool isFirstTime = prefs.getBool('isFirstTimeUser') ?? true;
+
+    if (isFirstTime) {
+      // 如果是第一次使用应用程序，将isFirstTimeUser设置为false
+      prefs.setBool('isFirstTimeUser', false);
+    }
+
+    return isFirstTime;
   }
 }

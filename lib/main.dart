e336333993fac257431796c2e5209940ff5e995a
ZylDa <a3842772@gmail.com>
2023-07-14 21:03:27 +0800

    7/14

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'screens/splash/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import './login_or_register_page.dart';
-import './home_page.dart';
+import 'package:swap/navigation.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
@@ -15,7 +15,7 @@ class AuthPage extends StatelessWidget {
         builder: (context, snapshot) {
           //user is logged in
           if (snapshot.hasData) {
-            return HomePage();
+            return const Navigation();
           }
           //user is not logged in
           else {
